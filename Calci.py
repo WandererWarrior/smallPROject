e67194249eef391d2927af181dc319950b82a49a
WandererWarrior <114157450+WandererWarrior@users.noreply.github.com>
2023-06-19 22:24:07 +0530

    Add files via upload

---
+++ Calci.py
@@ -0,0 +1,12 @@
+#A code for the calculator
+
+a= int(input("Enter 1st Number"))
+b= int(input("Enter 2nd Number"))
+Div = a/b
+Mul = a*b
+add = a+b
+Sub = a-b
+print("Division of the Numbers",Div)
+print("Multiplication of the Numbers",Mul)
+print("Addition is",add)
+print("Subtraction",Sub)
